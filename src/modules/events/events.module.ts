cf26709bc9e2ab27b2043b8ceee35f45ca7d5e78
Singh-Shashvat <shashvatsingh124@gmail.com>
2026-06-29 14:47:08 +0530

    feat: initialize events module with gateway and JWT authentication configuration

--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -3,10 +3,24 @@ import { EventsGateway } from './events.gateway';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WorkspaceMember } from '@/database/entities/workspace-member.entity';
+import { ConfigModule } from '../../core/config/config.module';
+import { EnvConfig } from '../../core/config/env.config';
 
 @Global()
 @Module({
-  imports: [JwtModule,TypeOrmModule.forFeature([WorkspaceMember])],
+  imports: [
+    ConfigModule,
+    JwtModule.registerAsync({
+      inject: [EnvConfig],
+      useFactory: async (envConfig: EnvConfig) => ({
+        secret: envConfig.jwtConfig.jwtSecret,
+        signOptions: {
+          expiresIn: envConfig.jwtConfig.jwtExpiry as any,
+        },
+      }),
+    }),
+    TypeOrmModule.forFeature([WorkspaceMember]),
+  ],
   providers: [EventsGateway],
   exports: [EventsGateway],
 })
